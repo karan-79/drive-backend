5e5b19c11965fd37a2f5b6fb837f83c02d06bc29
Karandeep-Singhh <ks7658917@gmail.com>
2025-05-31 21:54:45 +0530

    add docker file and deployble

--- src/test/java/com/project/mydrive/TestcontainersConfiguration.java
+++ src/test/java/com/project/mydrive/TestcontainersConfiguration.java
@@ -1,8 +1,6 @@
 package com.project.mydrive;
 
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
+
 
 }
